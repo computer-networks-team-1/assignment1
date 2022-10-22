2f5b77afa2fd127b93a8eb096eb3f8d86ed1912a
Francesco <francesco.mazzini432@gmail.com>
2022-10-22 22:16:14 +0200

    Started building GUI
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

---
+++ code/src/main/java/TCPClient/GUI/GUIRunner.java
@@ -0,0 +1,48 @@
+package TCPClient.GUI;
+
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+
+public class GUIRunner {
+
+    private GUIRunner() {}
+
+    /**
+     * Initializes JavaFX scene and loads fxml file.
+     *
+     * @param primaryStage JavaFX primary stage.
+     * @throws IOException when graphic.fxml is not found.
+     */
+    public static void run(Stage primaryStage) throws IOException {
+
+        primaryStage.setTitle("TCPClient - Online Chat");
+
+        Scene scene = new Scene(new VBox(), 850, 478);
+        scene.getStylesheets().add("style.css");
+
+        primaryStage.setScene(scene);
+        primaryStage
+                .getIcons()
+                .add(new Image(new File("src/main/resources/assets/logo.PNG").toURI().toString()));
+
+        primaryStage.setResizable(false);
+
+        primaryStage.setMaximized(false);
+        primaryStage.setFullScreen(false);
+        primaryStage.show();
+    }
+
+    /** Stops the GUI and the threads. */
+    public static void stop() {
+        Platform.exit();
+    }
+
+}
---
+++ code/src/main/java/TCPClient/GUI/Main.java
@@ -0,0 +1,14 @@
+package TCPClient.GUI;
+
+public class Main {
+
+    private Main() {}
+    /**
+     * Main method.
+     *
+     * @param args cli args.
+     */
+    public static void main(final String[] args) {
+        App.main(args);
+    }
+}
